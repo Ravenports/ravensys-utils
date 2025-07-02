5b2c0f4386a753e128c6576a287ca19021a3b347
jrmarino <draco@marino.st>
2025-07-02 01:30:31 -0500

    Remove decls from crc_extern.h

--- mtree/crc_extern.h
+++ mtree/crc_extern.h
@@ -1,6 +1,6 @@
-#include <sys/cdefs.h>
+#ifndef CRC_EXTERN_H
+#define CRC_EXTERN_H
 
-__BEGIN_DECLS
 int	crc(int, uint32_t *, off_t *);
 void	pcrc(char *, uint32_t, off_t);
 void	psum1(char *, uint32_t, off_t);
@@ -8,4 +8,5 @@ void	psum2(char *, uint32_t, off_t);
 int	csum1(int, uint32_t *, off_t *);
 int	csum2(int, uint32_t *, off_t *);
 int	crc32(int, uint32_t *, off_t *);
-__END_DECLS
+
+#endif
