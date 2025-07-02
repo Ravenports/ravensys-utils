b6ce958f9d627515e055606249d6dca70d19ed3e
jrmarino <draco@marino.st>
2025-07-02 01:41:57 -0500

    Drop another sys/cdefs, add sys/types

--- mtree/forsunos.h
+++ mtree/forsunos.h
@@ -2,6 +2,8 @@
 #define FORSUNOS_H
 
 #if defined(__sun__)
+#include <sys/types.h>
+
 char	 *fgetln(FILE *, size_t *);
 mode_t  getmode(const void *set, mode_t mode);
 void   *setmode(const char *mode_str);
